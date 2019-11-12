35310e1674678bc76be1ea53fb217a78c4fadf8f
Joseph Harburg <josephharburg@gmail.com>
2019-11-12 14:29:28 -0500

    refactored code

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -10,38 +10,23 @@ namespace Lab10
             //circumference and area.
             bool repeat = true;
             int num = 0;
-            bool success = true;
             Console.WriteLine("Welcome to the Circumference Calculator!\n");
             while (repeat)
             {
-                //try
-                //{
                     Console.WriteLine("Please enter a radius: ");
                     double radius = Validator.ValidateNumber();
                     Circle circle = new Circle(radius);
                     Console.WriteLine($"Circumference: {circle.CalculateFormattedCircumference()}\nArea: {circle.CalculateFormattedArea()}");
-                    //success = true;
                     repeat = Validator.Repeat();
-                //}
-                //catch (Exception)
-                //{
-                //    //Console.WriteLine("Im sorry thats not a valid repsonse!");
-                //    //repeat = true;
-                //    success = false;
-                //}
-                //if (success)
-                //{
                     num += 1;
-                //}
-                
             }
             if (num > 1)
             {
-                Console.WriteLine($"GoodBye! You created {num} cirlces!");
+                Console.WriteLine($"GoodBye!\nYou created {num} cirlces!");
             }
             else
             {
-                Console.WriteLine($"GoodBye! You created {num} cirlce!");
+                Console.WriteLine($"GoodBye!\nYou created {num} cirlce!");
             }
         }
     
